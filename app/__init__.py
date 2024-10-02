67baefaf09efa827980adf5c791b5fffc5d64101
Мельников Максим Антонович <max@sopka.ru>
2024-10-03 01:30:35 +0300

    IP handler and main app

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 
-from app.extensions import IPHandler
+from app.extensions import ip_handler
 from app.extensions import db
 from app.extensions import migrate
 from app.extensions import socketio
@@ -12,41 +12,28 @@ from app.models import db_models
 from app.blueprints import blueprints
 
 
-# Custom Flask app class with additional functionality
-class CustomFlaskApp(Flask):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ip_handler = None
-
-    def init_ip_handler(self):
-        self.ip_handler = IPHandler(self.config.get('GEOIP_DB'),
-                                    self.config.get('ALLOWED_COUNTRIES'))
-
-    # Function to load configuration based on environment
-    def load_config(self):
-        load_dotenv()
-        match os.getenv('FLASK_ENV'):
-            case 'development':
-                self.config.from_object('config.DevelopmentConfig')
-            case 'testing':
-                self.config.from_object('config.TestingConfig')
-            case 'production':
-                self.config.from_object('config.ProductionConfig')
-            case _:
-                self.config.from_object('config.DevelopmentConfig')
+def load_config(app):
+    load_dotenv()
+    env = os.getenv('FLASK_ENV', 'development')
+    if env == 'development':
+        app.config.from_object('config.DevelopmentConfig')
+    elif env == 'testing':
+        app.config.from_object('config.TestingConfig')
+    elif env == 'production':
+        app.config.from_object('config.ProductionConfig')
+    else:
+        app.config.from_object('config.DevelopmentConfig')
 
 
 # Factory function to create the Flask app
 def create_app():
-    app = CustomFlaskApp(__name__)
+    app = Flask(__name__)
 
-    app.load_config()
-
+    load_config(app)
     db.init_app(app)
     migrate.init_app(app, db)
     socketio.init_app(app)
-
-    app.init_ip_handler()
+    ip_handler.init_app(app)
 
     for bp in blueprints:
         app.register_blueprint(bp)
--- app/extensions/ip_handler.py
+++ app/extensions/ip_handler.py
@@ -1,64 +1,91 @@
-from flask import request, abort
-import geoip2.database, geoip2.errors
-from typing import Any, Union, List, Tuple
+from flask import Flask, request, abort
+import geoip2.database
+import geoip2.errors
+import threading
+import atexit
+from typing import Any, Union, List
+from functools import wraps
+
+
+def ensure_initialized(method):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if not self._initialized:
+            raise RuntimeError("IPHandler is not initialized. Call `init` or `init_app` beforehand.")
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class IPHandler:
     _instance = None
+    _initialized = False
+    _lock = threading.Lock()
 
-    def __new__(cls, geoip_db: Any = None, allowed_countries: Union[str, List[str], Tuple[str]] = 'all'):
+    def __new__(cls):
         if cls._instance is None:
             cls._instance = super(IPHandler, cls).__new__(cls)
-            cls._instance.geoip_db = geoip_db
-            cls._instance.allowed_countries = allowed_countries
-            cls._instance.ip_address = ''
-            cls._instance.country_code = ''
-            cls._instance.local = True
-            cls._instance.error = False
         return cls._instance
 
+    def __init__(self):
+        self.geoip_db = ''
+        self.allowed_countries = 'all'
+        self.ip_address = ''
+        self.country_code = ''
+        self.local = True
+        self.error = False
+        self.geoip_reader = None
+
+    def init_app(self, app: Flask):
+        geoip_db = app.config.get('GEOIP_DB')
+        allowed_countries = app.config.get('ALLOWED_COUNTRIES')
+        self.init(geoip_db, allowed_countries)
+
+    def init(self, geoip_db: str = None, allowed_countries: Union[str, List[str]] = 'all'):
+        with self._lock:
+            if not self._initialized:
+                self.geoip_db = geoip_db
+                self.allowed_countries = [allowed_countries] if isinstance(allowed_countries, str) else list(allowed_countries)
+                self.geoip_reader = geoip2.database.Reader(self.geoip_db)
+                atexit.register(self.cleanup)
+                IPHandler._initialized = True
+
+    @ensure_initialized
     def update_client_info(self):
-        self.get_client_ip()
-        self.get_client_country()
+        with self._lock:
+            self.get_client_ip()
+            self.get_client_country()
 
+    @ensure_initialized
     def get_client_ip(self):
-        # Check for the 'X-Forwarded-For' header (used in case of proxies or load balancers)
-        if 'X-Forwarded-For' in request.headers:
-            # It's a comma-separated list of IPs; the first one is the original client IP
-            client_ip = request.headers['X-Forwarded-For'].split(',')[0].strip()
-        else:
-            # Fallback to remote_addr if no proxy is involved
-            client_ip = request.remote_addr
+        client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+        if client_ip:
+            client_ip = client_ip.split(',')[0].strip()
 
-        if not client_ip:
-            self._instance.error = True
+        self.local = client_ip in ('127.0.0.1', '::1')
+        self.ip_address = client_ip if client_ip else ''
 
-        if client_ip != '127.0.0.1':
-            self._instance.local = False
+        if not self.ip_address:
+            self.error = True
 
-        self._instance.ip_address = client_ip
-
+    @ensure_initialized
     def get_client_country(self):
         try:
-            with geoip2.database.Reader(self.geoip_db) as geoip_reader:
-                response = geoip_reader.country(self.ip_address)
-                self._instance.country_code = response.country.iso_code
-
+            response = self.geoip_reader.country(self.ip_address)
+            self.country_code = response.country.iso_code
         except geoip2.errors.AddressNotFoundError:
-            self._instance.error = True
+            self.error = True
 
-    def check_allowed_countries(self) -> bool:
-        if isinstance(self.allowed_countries, str):
-            if self.allowed_countries == 'all':
-                return True
+    @ensure_initialized
+    def is_country_allowed(self) -> bool:
+        return 'all' in self.allowed_countries or self.country_code in self.allowed_countries
 
-        if self.country_code not in self.allowed_countries:
-            return False
-
-        return True
-
+    @ensure_initialized
     def check_ip(self):
         self.update_client_info()
-        if not self.local:
-            if not self.check_allowed_countries():
-                abort(403, "IP from your country is not allowed.")
+        if not self.local and not self.is_country_allowed():
+            abort(403, "IP from your country is not allowed.")
+
+    @ensure_initialized
+    def cleanup(self):
+        if self.geoip_reader:
+            self.geoip_reader.close()
